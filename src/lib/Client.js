92e4e36a05eebbf1e898b8da9387cdd3b4c92f39
gc <30398469+gc@users.noreply.github.com>
2021-06-01 04:59:28 +1000

    fix

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -328,7 +328,7 @@ class KlasaClient extends Discord.Client {
 	get owners() {
 		const owners = new Set();
 		for (const owner of this.options.owners) {
-			const user = this.users.get(owner);
+			const user = this.users.cache.get(owner);
 			if (user) owners.add(user);
 		}
 		return owners;
