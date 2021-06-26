42efcdfcffa67634c6943219ecbb80c531c6a827
gc <30398469+gc@users.noreply.github.com>
2021-06-27 08:36:10 +1000

    fix argument err bug

--- src/events/argumentError.js
+++ src/events/argumentError.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, params, error = 'Unknown error') {
-		const errorMessage = typeof err === 'string' ? error : error.message || 'Unknown error';
+		const errorMessage = typeof error === 'string' ? error : error.message || 'Unknown error';
 		message.sendMessage(errorMessage).catch(err => this.client.emit('wtf', err));
 	}
 
