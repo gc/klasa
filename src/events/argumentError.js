d21b7f28f2488e145828850f7941e0f76661b7ee
gc <30398469+gc@users.noreply.github.com>
2021-06-27 17:56:44 +1000

    change argument err handling

--- src/events/argumentError.js
+++ src/events/argumentError.js
@@ -3,8 +3,12 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, params, error = 'Unknown error') {
-		const errorMessage = typeof error === 'string' ? error : error.message || 'Unknown error';
-		message.sendMessage(errorMessage).catch(err => this.client.emit('wtf', err));
+		if (typeof error !== 'string') {
+			console.error(error);
+			message.sendMessage(`An unexpected error occured.`).catch(err => this.client.emit('wtf', err));
+		} else {
+			message.sendMessage(error).catch(err => this.client.emit('wtf', err));
+		}
 	}
 
 };
