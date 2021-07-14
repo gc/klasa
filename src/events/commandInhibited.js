21b08370e3567bf40a707fcbe4e4d179ce2c95f1
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-07-13 20:48:48 -0700

    Fixes inhibitor errors

--- src/events/commandInhibited.js
+++ src/events/commandInhibited.js
@@ -3,7 +3,13 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, response) {
-		if (response && response.length) message.channel.send(response);
+		if (response && response.length) {
+			if (Array.isArray(response)) {
+				message.channel.send({ content: response.join('\n') });
+			} else {
+				message.channel.send({ content: response });
+			}
+		}
 	}
 
 };
